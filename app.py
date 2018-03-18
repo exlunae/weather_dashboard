bb1f0760235ee9883f98bbf2885ee12b398c9144
Jonathan Wong <chi.jonathanwong@gmail.com>
2018-03-17 18:34:21 -0700

    Add boilerplate

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, redirect, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return redirect('/index')
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/info', methods=['POST', 'GET'])
+def info():
+    # if request.method == 'POST':
+    info = request.form
+    return render_template('info.html', info=info)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
